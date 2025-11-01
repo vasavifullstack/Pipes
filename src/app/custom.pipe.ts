531b07d182c5061f04c3c0eacfebb857670ccd40
vasavifullstack <vasavi.fullstack@gmail.com>
2025-11-01 15:12:02 +0530

    add

--- src/app/custom.pipe.ts
+++ src/app/custom.pipe.ts
@@ -5,9 +5,20 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class CustomPipe implements PipeTransform {
 
-  transform(value:any) {
-    // console.log(value);
-   return value.slice(0,20);
+  // transform(value:any) {
+  //   // console.log(value);
+  //   if(value.length>=40){
+  //  return value.slice(0,20)+'...';
+  // }
+  // return value;
+// }
+//value refers content,limit refers length ,
+// symbol refers symbols like '...','***'
+transform(value:any,limit?:any,symbol?:any){
+  if(value.length>limit){
+    return value.slice(0,limit)+symbol;
   }
+  return value;
+}
 
 }
